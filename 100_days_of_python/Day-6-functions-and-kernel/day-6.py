5a29e53e357896e150346d55ca55676525ec2336
Scott Le <scottyle02@outlook.com>
2025-01-24 02:24:08 -0500

    Day 7

--- 100_days_of_python/Day-6-functions-and-kernel/day-6.py
+++ 100_days_of_python/Day-6-functions-and-kernel/day-6.py
@@ -5,3 +5,29 @@ def myfunction():
     print("Hello")
     print("Bye")
 
+myfunction()
+
+#While loops 
+"""Comparing for loops vs while loops. 
+
+for item in list_of_items:
+    #Do something to each item 
+
+for number in range(a,b):
+    print(number)
+
+Two flavors of loops, one where we're looping through a list of items to do something to each item in the list and the other 
+is using a range function where we create a range and we use every number in that range to do something 
+
+while something_is_true:
+    #Do something repeatedly 
+
+The while loop basically means while the particular condition is true, do something till it is false 
+
+When should you use a for loop vs a while loop. 
+Loops are great for when you want to iterate over something and you need to do something to each thing you're
+iterating over 
+
+While loops are good for when you don't care what number of sequence you're in and want it to lop till the condition you set 
+becomes True
+"""
