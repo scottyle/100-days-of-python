d915de4052624dcdd5c5cbe295cfcc8b1b2f39cb
Scott Le <scottyle02@outlook.com>
2025-05-11 20:31:03 -0400

    Day-17

---
+++ 100_days_of_python/Day-17-The-quiz-project&benefits-of-oop/day-17-p2.py
@@ -0,0 +1,11 @@
+#Note for classes use PascalCase naming convention, first letter capitalized 
+#We use PascalCase and snake_case naming conventions 
+
+class User:
+    pass
+
+user_1 = User() 
+user_1.id = "001"
+user_1.username = "Scott"
+
+print(user_1.id)
