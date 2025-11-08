721d147ed6e8f6de4a56cb9d21014279ce815a45
Scott Le <scottyle02@outlook.com>
2025-11-07 19:50:56 -0500

    Day-28

---
+++ 100_days_of_python/Day-28/main.py
@@ -0,0 +1,53 @@
+from tkinter import * 
+from tkmacosx import Button
+import math 
+
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    count_down(WORK_MIN*60)
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+    count_min = math.floor(count/60)
+    count_sec = count%60
+    canvas.itemconfig(timer_text,text=f"{count_min}:{count_sec}")
+    if count > 0:
+        window.after(1000,count_down,count-1)
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomorodo timer")
+window.config(padx=100,pady=50,bg=YELLOW)
+
+canvas = Canvas(width=200,height=224,bg=YELLOW,highlightthickness=0)
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(100,112,image=tomato_img)
+timer_text = canvas.create_text(100,130, text="00:00", fill = "white", font=(FONT_NAME,35,"bold"))
+canvas.grid(row=1,column=1)
+
+timer_label = Label(text="Timer", fg=GREEN,bg=YELLOW, font=(FONT_NAME,100,"bold"))
+timer_label.grid(row=0,column=1)
+
+start_button = Button(text="Start",bg=YELLOW,activebackground=YELLOW,borderless=1,command=start_timer)
+start_button.grid(row=2,column=0)
+
+
+reset_button = Button(text="Reset",bg=YELLOW,activebackground=YELLOW,borderless=1)
+reset_button.grid(row=2,column=3)
+
+tracker = Label(text="✅",fg=GREEN,bg=YELLOW)
+tracker.grid(row=3,column=1)
+
+window.mainloop()
