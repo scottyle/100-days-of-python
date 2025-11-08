bfe0bf9205a8be332a10b502ad8162f6df6e6ebf
Scott Le <scottyle02@outlook.com>
2025-11-08 01:35:49 -0500

    Day-28 P2

--- 100_days_of_python/Day-28/main.py
+++ 100_days_of_python/Day-28/main.py
@@ -8,23 +8,47 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
+WORK_MIN = 1
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+REPS = 0
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
-    count_down(WORK_MIN*60)
+    global REPS
+    REPS += 1
+
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60 
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if REPS % 8 == 0:
+        count_down(long_break_sec)
+        timer_label.config(text="Break",fg=RED)
+    elif REPS % 2 == 0:
+        count_down(short_break_sec)
+        timer_label.config(text="Break",fg=PINK)
+    else: 
+        count_down(work_sec)
+        timer_label.config(text="Work")
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def count_down(count):
     count_min = math.floor(count/60)
     count_sec = count%60
+
+    if count_sec <= 9: 
+        count_sec = f"0{count_sec}"
+    if count_sec == 0:
+        count_sec = "00"
+
     canvas.itemconfig(timer_text,text=f"{count_min}:{count_sec}")
     if count > 0:
         window.after(1000,count_down,count-1)
+    else: 
+        start_timer()
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
