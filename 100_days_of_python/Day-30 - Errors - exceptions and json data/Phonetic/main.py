6015c5b237338e689910955c7dd84d0bc0c5b5db
Scott Le <scottyle02@outlook.com>
2025-11-11 01:57:33 -0500

    Day-30 start

---
+++ 100_days_of_python/Day-30 - Errors - exceptions and json data/Phonetic/main.py
@@ -0,0 +1,19 @@
+import pandas
+
+def phoneticDict():
+    while True:
+        try:
+            word = input("Enter a word: ").upper()
+            output_list = [phonetic_dict[letter] for letter in word]
+        except KeyError:
+            print(f"Sorry only letters in the alphabet please.")
+        else:
+            print(output_list)
+            break
+
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+
+phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+print(phonetic_dict)
+
+phoneticDict()
