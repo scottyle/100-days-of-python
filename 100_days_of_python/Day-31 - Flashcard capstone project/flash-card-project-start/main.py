e4f1cc686f092259dfd253efb804a6f97de9fa9a
Scott Le <scottyle02@outlook.com>
2025-11-12 22:23:55 -0500

    Working part 3

--- 100_days_of_python/Day-31 - Flashcard capstone project/flash-card-project-start/main.py
+++ 100_days_of_python/Day-31 - Flashcard capstone project/flash-card-project-start/main.py
@@ -10,42 +10,52 @@ CARD_FRONT_IMG = "images/card_front.png"
 BACKGROUND_COLOR = "#B1DDC6"
 FONT_NAME = "Arial"
 
-# ----------------------------CREATE NEW FLASHCARDS ------------------------------- #
-def create_new_flash_cards():
-    try: 
-        df = pd.read_csv(FRENCH_WORDS_DATA)
-    except FileNotFoundError as e:
-        print(e)
-    else:
-        words = df.to_dict()
-        length_of_french_words = len(words["French"])
-        #Loop through dict to get both french words and english words 
-        canvas.itemconfigure(title_text,text="French")
-        canvas.itemconfigure(word_text,text=words["French"][random.randrange(0,length_of_french_words)])
+# ---------------------------- CREATE NEW FLASHCARDS ------------------------------- #
+def create_new_words():
+    current_card = random.choice(words)
+    canvas.itemconfig(canvas_image,image=card_front_img)
+    canvas.itemconfigure(title_text,text="French")
+    canvas.itemconfigure(word_text,text=current_card["French"])
+    window.after(3000,lambda: flip_card(current_card["English"]))
 
+# ---------------------------- FLIP CARD TO ENGLISH WORD ------------------------------- #
+def flip_card(current_word):
+    canvas.itemconfig(canvas_image,image=card_back_img)
+    canvas.itemconfig(title_text,text="English")
+    canvas.itemconfigure(word_text,text=current_word)
+
+# ---------------------------- READ DATA OF WORDS ------------------------------- #
+try: 
+    df = pd.read_csv(FRENCH_WORDS_DATA)
+    words = df.to_dict(orient="records")
+except FileNotFoundError as e:
+    print("Cannot find the french_words.csv file.")
+        
 # ---------------------------- UI SETUP ------------------------------- #
-window = Tk()
-window.title(string="Flashy")
-window.config(padx=50,pady=50,bg=BACKGROUND_COLOR)
+#Run only if the csv file does not throw an error. 
+else:
+    window = Tk()
+    window.title(string="Flashy")
+    window.config(padx=50,pady=50,bg=BACKGROUND_COLOR)
 
-canvas = Canvas(width=800,height=526)
-card_front_img = PhotoImage(file=CARD_FRONT_IMG)
-canvas.create_image(400,263,image=card_front_img)
-canvas.config(bg=BACKGROUND_COLOR,highlightthickness=0)
-canvas.grid(row=0,column=0,columnspan=2)
+    canvas = Canvas(width=800,height=526)
+    card_front_img = PhotoImage(file=CARD_FRONT_IMG)
+    card_back_img = PhotoImage(file=CARD_BACK_IMG)
+    canvas_image = canvas.create_image(400,263,image=card_front_img)
+    canvas.config(bg=BACKGROUND_COLOR,highlightthickness=0)
+    canvas.grid(row=0,column=0,columnspan=2)
 
-title_text = canvas.create_text(400,150, text="Title",font=(FONT_NAME,40))
-word_text = canvas.create_text(400,263,text="Word", font=(FONT_NAME,60,"bold"))
+    title_text = canvas.create_text(400,150, text="Title",font=(FONT_NAME,40))
+    word_text = canvas.create_text(400,263,text="Word", font=(FONT_NAME,60,"bold"))
 
-#Buttons
-right_img = PhotoImage(file=RIGHT_IMG)
-right_button = Button(image=right_img, highlightthickness=0,command=create_new_flash_cards)
-right_button.grid(row=1,column=1)
+    #Buttons
+    right_img = PhotoImage(file=RIGHT_IMG)
+    right_button = Button(image=right_img, highlightthickness=0,command=create_new_words)
+    right_button.grid(row=1,column=1)
 
-wrong_img = PhotoImage(file=WRONG_IMG)
-wrong_button = Button(image=wrong_img,highlightthickness=0,command=create_new_flash_cards)
-wrong_button.grid(row=1,column=0)
+    wrong_img = PhotoImage(file=WRONG_IMG)
+    wrong_button = Button(image=wrong_img,highlightthickness=0,command=create_new_words)
+    wrong_button.grid(row=1,column=0)
 
 
-
-window.mainloop()   
+    window.mainloop()   
