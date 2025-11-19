c6fe4e7f51f091a23172ae11a91064d605c360ba
Scott Le <scottyle02@outlook.com>
2025-11-19 00:49:15 -0500

    Day-33-completed

--- 100_days_of_python/Day-33-APIs-endpoints/issoverhead-start/main.py
+++ 100_days_of_python/Day-33-APIs-endpoints/issoverhead-start/main.py
@@ -1,9 +1,11 @@
 import requests
 from datetime import datetime
-import math
+import smtplib
 
-# MY_LAT = 43.653225 
-# MY_LONG = -79.383186 
+MY_LAT = 43.653225 
+MY_LONG = -79.383186 
+MY_EMAIL = "@gmail.com"
+PASSWORD = ""
 
 parameters = {
     "lat": MY_LAT,
@@ -21,7 +23,7 @@ def return_iss_position():
 
     iss_latitude = float(data["iss_position"]["latitude"])
     iss_longitude = float(data["iss_position"]["longitude"])
-    return iss_latitude,iss_longitude
+    return iss_latitude, iss_longitude
 
 def get_sunrise_and_sunset():
     """
@@ -32,18 +34,34 @@ def get_sunrise_and_sunset():
     data = response.json()
     sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
     sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
-    return sunrise,sunset
+    return sunrise, sunset
+
+def send_notification():
+    """
+    Function sends a notification to target email, 
+    """
+    with smtplib.SMTP(host="smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=MY_EMAIL,password=PASSWORD)
+        try:
+            connection.sendmail(
+                from_addr=MY_EMAIL,
+                to_addrs="@gmail.com",
+                msg=f"Subject:Lookup\n\nThe space station is right above you.")
+        except smtplib.SMTPRecipientsRefused as e:
+            print(f"Failed to send email:{e}")
 
 iss_latitude,iss_longitude = return_iss_position()
-sunrise,sunset = get_sunrise_and_sunset()
-
 time_now = datetime.now()
+#Assume time is in UTC 
 hour = time_now.hour
 
-
 #Check if your position is within +5 or -5 degrees of the ISS position and it is currently dark
-if ((MY_LAT - abs(iss_latitude)) <= 5 and (abs(MY_LONG)-abs(iss_longitude))) and sunset <= hour <= sunrise:
-    print("works") 
+if (-5 <= MY_LAT - iss_latitude <= 5) and (-5 <= MY_LONG - iss_longitude <= 5):
+    #Check if it is currently dark: 
+    sunrise,sunset = get_sunrise_and_sunset()
+    if sunrise <= hour <= sunset:
+        send_notification()
 
 
 #If the ISS is close to my current position
