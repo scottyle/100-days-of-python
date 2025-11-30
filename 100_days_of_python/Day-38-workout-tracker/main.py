db1a2d1727ca9e9361af5454e5e2aae730cac3aa
Scott Le <scottyle02@outlook.com>
2025-11-30 17:27:11 -0500

    Updated Day-38

--- 100_days_of_python/Day-38-workout-tracker/main.py
+++ 100_days_of_python/Day-38-workout-tracker/main.py
@@ -4,22 +4,26 @@ import os
 #Third-Part imports
 import requests 
 from dotenv import load_dotenv
+from datetime import datetime
 
 load_dotenv()
 
 APP_ID = os.getenv("WORKOUT_APP_ID")
 API_KEY = os.getenv("WORKOUT_API_KEY")
+SHEETY_URL = "https://api.sheety.co/56e6cb3fefaa69b61b1dc5bb0c57e5a5/myWorkouts/workouts"
 EXERCISE_URL = "https://app.100daysofpython.dev"
+WEIGHT_KG = 67.5
+HEIGHT_CM = 175 
+AGE = 31 
+GENDER = "male" 
 
-exercise_data = {
+today = datetime.today()
+todays_date = today.strftime("%d/%m/%Y")
+todays_time = today.strftime("%H:%M:%S")
 
-    "query": "ran 3 miles",
-    "weight_kg": 70,                
-    "height_cm": 175,             
-    "age": 30,                      
-    "gender": "male",
+exercise_input = input("Tell me which exercises you did: ")
 
-}
+
 
 headers = {
     "Content-type" : "application/json",
@@ -27,6 +31,29 @@ headers = {
     "x-app-key" : API_KEY
 }
 
-response = requests.post(url=f"{EXERCISE_URL}/v1/nutrition/natural/exercise", json = exercise_data, headers=headers )
-response.raise_for_status
-print(response.text)
+def add_workout_to_google_sheet(workout_details:dict):
+    """Takes the input of workout details and creates a entry to My Workouts"""
+    
+    response = requests.post(url=SHEETY_URL, json = workout_details)
+
+def get_workout_details():
+    """"""
+
+    exercise_data = {
+
+    "query": exercise_input,
+    "weight_kg": WEIGHT_KG,                
+    "height_cm": HEIGHT_CM,             
+    "age": AGE,                      
+    "gender": GENDER,
+
+    }   
+    
+    response = requests.post(url=f"{EXERCISE_URL}/v1/nutrition/natural/exercise", json = exercise_data, headers=headers )
+    response.raise_for_status
+    data = response.json()
+    print(response.text)
+
+breakpoint()
+
+
